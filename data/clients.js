a89a0d4e73f933d0b2e7149f876a56752d275aac
stephvm11 <00056024@uca.edu.sv>
2025-10-23 22:19:55 -0600

    feat: Agregar archivos para creacion de endpoints.

---
+++ data/clients.js
@@ -0,0 +1,58 @@
+export const clients = [
+  {
+    "_id": "68fabea41668f5f8ee38d681",
+    "isActive": true,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$2,408.40",
+    "client": "Weeks Poole",
+    "gender": "male"
+  },
+  {
+    "_id": "68fabea48fdeae3c4604526c",
+    "isActive": false,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$3,341.40",
+    "client": "Dennis Lucas",
+    "gender": "male"
+  },
+  {
+    "_id": "68fabea40d283854bbdb011b",
+    "isActive": true,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$3,181.77",
+    "client": "Flora Harrell",
+    "gender": "female"
+  },
+  {
+    "_id": "68fabea469ea5adec1f55628",
+    "isActive": true,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$2,940.78",
+    "client": "Chase James",
+    "gender": "male"
+  },
+  {
+    "_id": "68fabea41057997ccfaf73dd",
+    "isActive": false,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$3,465.83",
+    "client": "Anastasia Dillon",
+    "gender": "female"
+  },
+  {
+    "_id": "68fabea4026111ea21423d09",
+    "isActive": false,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$3,272.16",
+    "client": "Belinda Bryant",
+    "gender": "female"
+  },
+  {
+    "_id": "68fabea4ec3ab10a2fd7ab6a",
+    "isActive": true,
+    "picture": "http://placehold.it/32x32",
+    "balance": "$1,385.48",
+    "client": "Ellis Montgomery",
+    "gender": "male"
+  }
+]
---
+++ parcial2.js
@@ -0,0 +1,85 @@
+const app = express();
+import express from "express";
+import { clients } from "./data/clients.js";
+const port = 3130;
+
+// endpoint 1 y 3.
+app.get("/cuentas", (req, res) => {
+  const { id, client, gender } = req.query;
+
+  if (id || client || gender) {
+    let results = [];
+    if (id) {
+      results = clients.filter((account) => account._id === id);
+    } else if (client) {
+      const queryLower = client.toLowerCase();
+      results = clients.filter((account) => account.client === queryLower);
+    } else if (gender) {
+      const queryLower = gender.toLowerCase();
+      results = clients.filter(
+        (account) => account.gender.toLowerCase() === queryLower
+      );
+    }
+
+    let response;
+    if (results.length === 1) {
+      response = {
+        found: true,
+        account: results[0],
+      };
+    } else if (results.length > 1) {
+      response = {
+        found: true,
+        data: results,
+      };
+    } else {
+      response = {
+        found: false,
+        data: "Cuentas no encontradas.",
+      };
+    }
+    res.send(response);
+  } else {
+    const response = {
+      count: clients.length,
+      data: clients,
+    };
+    res.send(response);
+  }
+});
+
+// endpoint 2.
+app.get("/cuentas/:id", (req, res) => {
+  const { id } = req.params;
+  const client = clients.find((b) => b._id === id);
+  const response = {
+    finded: !!client,
+    account: client || "Cuenta no encontrada.",
+  };
+  res.send(response);
+});
+
+// endpoint 4.
+app.get("/cuentasBalance", (req, res) => {
+  const activeAccounts = clients.filter((account) => account.isActive === true);
+  const active = activeAccounts.length > 0;
+  let totalBalance = 0;
+
+  if (active) {
+    totalBalance = activeAccounts.reduce((sum, account) => {
+      const balance = parseFloat(account.balance.replace(/[$,]/g, ""));
+      return sum + balance;
+    }, 0);
+  }
+
+  const response = {
+    status: active,
+    accountBalance: totalBalance
+  };
+
+  res.send(response);
+});
+
+app.listen(port, () => {
+  console.log(`API escuchando en http://localhost:${port}`);
+});
